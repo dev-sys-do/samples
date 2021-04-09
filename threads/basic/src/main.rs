109ddba1e68e39a84cf676ede5ce2ee4b820729b
Samuel Ortiz <sameo@linux.intel.com>
2021-04-09 05:27:25 +0200

    basic: Add vector move and timeout change
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- threads/basic/src/main.rs
+++ threads/basic/src/main.rs
@@ -2,13 +2,17 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let handle = thread::spawn(|| {
-        println!("THREAD: Zzzzz....");
-        thread::sleep(Duration::from_millis(500));
-        println!("THREAD: Woke up");
+    let mut timeout = 500;
+    let mut vector = vec!["hello"];
+    println!("{:?}", vector);
+
+    let handle = thread::spawn(move || {
+        timeout = 5000;
+        vector.push("world!");
+        thread::sleep(Duration::from_millis(timeout));
+        println!("Thread is done (timeout {} vector {:?})", timeout, vector);
     });
 
-    println!("MAIN: Waiting for our spawned thread");
+    println!("MAIN: Waiting for our spawned thread (timeout {})", timeout);
     handle.join().unwrap();
-    println!("MAIN: Thread is done");
 }
