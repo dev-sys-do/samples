91722dfc4daff4a270091d5132654f38db8358e0
Samuel Ortiz <sameo@linux.intel.com>
2021-05-27 00:36:22 +0200

    networking: tcp: Read and write from the client
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- networking/tcp/client/src/main.rs
+++ networking/tcp/client/src/main.rs
@@ -1,7 +1,17 @@
+use std::io::{Read, Write};
 use std::net::TcpStream;
 
 fn main() -> std::io::Result<()> {
-    let client = TcpStream::connect("127.0.0.1:8080")?;
+    let mut client = TcpStream::connect("127.0.0.1:8080")?;
+    client.write(b"GET / HTTP/1.1")?;
+
+    let mut server_data = [0; 1024];
+    client.read(&mut server_data)?;
+
+    println!(
+        "Server data:\n{}",
+        String::from_utf8_lossy(&server_data[..])
+    );
 
     Ok(())
 }
